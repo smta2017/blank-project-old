dbe231952d597c4cd36983f0850180a5e28de31c
sameh <smta55@hotmail.com>
2021-10-25 23:49:27 +0200

    edit City model relation with area

--- app/Models/City.php
+++ app/Models/City.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class City extends Model
+class City extends Model implements Auditable
 {
     use HasFactory;
+    use \OwenIt\Auditing\Auditable;
+
+    public function Areas()
+    {
+        $this->hasMany(Area::class);
+    }
 }
